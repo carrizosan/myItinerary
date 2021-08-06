244068788f37db8bfad95f366561531603f44b80
carrizosan <santiicaarrizo@gmail.com>
2021-08-06 16:02:57 -0300

    Refactor sprint 1

---
+++ server/controllers/city/cityController.js
@@ -0,0 +1,4 @@
+const get = require("./getCities/getCities");
+const create = require("./createCity/createCity");
+
+module.exports = { get, create };
--- server/models/cityModel.js
+++ server/models/cityModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cityScheema = mongoose.Schema({
   name: {
     type: String,
+    unique: true,
     required: true,
   },
   country: {
